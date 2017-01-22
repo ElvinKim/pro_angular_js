375e18b4b13fa7989d47e85fa3dd9835603ae4dd
ElvinKim <bonwho09@gmail.com>
2017-01-23 00:37:29 +0900

    [edit][apply] $http.get

--- sportsstore/angularjs/controllers/sportsStore.js
+++ sportsstore/angularjs/controllers/sportsStore.js
@@ -1,32 +1,13 @@
 angular.module("sportsStore")
-	.controller("sportsStoreCtrl", function($scope){
+	.constant("dataUrl", "http://localhost:5050/products")
+	.controller("sportsStoreCtrl", function($scope, $http, dataUrl){
 
-		$scope.data = {
-			products : [
-				{
-					name : "Product #1",
-					description : "A product",
-					category : "Category #1",
-					price : 100
-				},
-				{
-					name : "Product #2",
-					description : "A product",
-					category : "Category #1",
-					price : 110
-				},
-				{
-					name : "Product #3",
-					description : "A product",
-					category : "Category #2",
-					price : 210
-				},
-				{
-					name : "Product #4",
-					description : "A product",
-					category : "Category #3",
-					price : 202
-				}
-			]
-		}
+		$scope.data = {};
+
+		$http.get(dataUrl)
+			.then(function(response) {
+				$scope.data.products = response.data;
+			}, function(response){
+				$scope.data = response;
+			});
 	});
